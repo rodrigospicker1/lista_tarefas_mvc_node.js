43811c5742a9905b0529d3e546b654d4e8f5ef3a
rodrigospicker1 <r.spicker13@gmail.com>
2023-03-04 15:31:54 -0300

    design_add_tarefa

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const conn = require('./db/conn')
 const Task = require('./models/Task')
+const tasksRoutes = require('./routes/taskRoutes')
 
 const app = express()
 
@@ -9,15 +10,15 @@ conn.sync().then(() => {
     app.listen(3000)
 }).catch((err) => console.log(err))
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-)
-
-app.use(express.json())
 
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+app.use(express.json())
+app.use('/tasks', tasksRoutes)
